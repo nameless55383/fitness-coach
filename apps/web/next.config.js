2582242d12ea21a820e68b0ed02b96e2e903e316
Avelyn <avelxr0426@gmail.com>
2026-05-09 14:13:43 +0800

    fix: enable static export for Netlify deployment

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
+  output: "export",
 };
 
 module.exports = nextConfig;
